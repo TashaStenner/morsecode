4526e6991d665d0305cb287e7d749fbab6dd11cf
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-02-26 16:09:43 +0000

    it-works

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const alphabet = {
     a: ".-",
     b: "-...",
@@ -40,11 +38,11 @@ const alphabet = {
     "0": "-----"
   };
   
-  const translate = () => {
-    const input = document.getElementById("input");
+  const translateEnglishToMorseCode = () => {
+    const input = document.getElementById("englishInput");
     const inputValue = input.value;
     // "Matt"
-    const output = document.getElementById("output");
+    const output = document.getElementById("morseOutput");
   
     const characters = inputValue.toLowerCase().split("");
     // characters = ["m", "a", "t", "t"]
@@ -56,7 +54,65 @@ const alphabet = {
     output.innerHTML = morseString;
   };
   
-  document.getElementById("translate").addEventListener("click", translate);
+  document.getElementById("translateEnglishToMorse").addEventListener("click", translateEnglishToMorseCode);
+
+  const morseAlphabet = {
+    ".-":"a",
+    "-...":"b",
+    "-.-.":"c",
+    "-..":"d",
+    ".":"e",
+    "..-.":"f",
+    "--.":"g",
+    "....":"h",
+    "..":"i",
+    ".---":"j",
+    "-.-":"k",
+    ".-..":"l",
+    "--":"m",
+    "-.":"n",
+    "---": "o",
+    ".--.":"p",
+    "--.-":"q",
+    ".-.":"r",
+    "...":"s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-":"x",
+    "-.--":"y",
+    "--..":"z",
+    " ": " ",
+    ".----":"1",
+    "..---":"2",
+    "...--":"3",
+    "....-":"4",
+    ".....":"5",
+    "-....":"6",
+    "--...":"7",
+    "---..":"8",
+    "----.":"9",
+    "-----":"0",
+  };
+
+  const translateMorseToEnglish = () => {
+    const MorseInput = document.getElementById("morseInput");
+    const morseInputValue = MorseInput.value;
+    // "Matt"
+    const englishOutput = document.getElementById("enOutput");
+  
+    const morseCharacters = morseInputValue.split(" ");
+    // characters = ["m", "a", "t", "t"]
+    const englishCharacters = morseCharacters.map(char => morseAlphabet[char]);
+    // ["--", "..-"]
+    const englishString = englishCharacters.join("");
+    // "-- ..-"
+  
+    englishOutput.innerHTML = englishString;
+  };
+  
+  document.getElementById("translateMorseCodeToEnglish").addEventListener("click", translateMorseToEnglish);
 
 // const compareTextToArray = (morseCode, displayText) => {
 
