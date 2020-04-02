20a788991002435a56c65526b8a612ff3685e87f
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-04-02 11:23:30 +0100

    delete-commeted-out-code

--- script.js
+++ script.js
@@ -1,143 +1,119 @@
 const alphabet = {
-    a: ".-",
-    b: "-...",
-    c: "-.-.",
-    d: "-..",
-    e: ".",
-    f: "..-.",
-    g: "--.",
-    h: "....",
-    i: "..",
-    j: ".---",
-    k: "-.-",
-    l: ".-..",
-    m: "--",
-    n: "-.",
-    o: "---",
-    p: ".--.",
-    q: "--.-",
-    r: ".-.",
-    s: "...",
-    t: "-",
-    u: "..-",
-    v: "...-",
-    w: ".--",
-    x: "-..-",
-    y: "-.--",
-    z: "--..",
-    " ": " ",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "0": "-----"
-  };
-  
-    const morseAlphabet = {
-      ".-":"a",
-      "-...":"b",
-      "-.-.":"c",
-      "-..":"d",
-      ".":"e",
-      "..-.":"f",
-      "--.":"g",
-      "....":"h",
-      "..":"i",
-      ".---":"j",
-      "-.-":"k",
-      ".-..":"l",
-      "--":"m",
-      "-.":"n",
-      "---": "o",
-      ".--.":"p",
-      "--.-":"q",
-      ".-.":"r",
-      "...":"s",
-      "-": "t",
-      "..-": "u",
-      "...-": "v",
-      ".--": "w",
-      "-..-":"x",
-      "-.--":"y",
-      "--..":"z",
-      " ": " ",
-      ".----":"1",
-      "..---":"2",
-      "...--":"3",
-      "....-":"4",
-      ".....":"5",
-      "-....":"6",
-      "--...":"7",
-      "---..":"8",
-      "----.":"9",
-      "-----":"0",
-    };
-  
-  const translateEnglishToMorseCode = () => {
-    const inputValue = input().value;
-    
-    const output = document.getElementById("morseOutput");
-  
-    const characters = inputValue.toLowerCase().split("");
- 
-    const morseCharacters = characters.map(char => alphabet[char]);
-    
-    const morseString = morseCharacters.join(" ");
-    
-    output.innerHTML = morseString;
-  };
-  
-  const input = () =>  document.getElementById("englishInput");
-  
-  
-  document.getElementById("translateEnglishToMorse").addEventListener("click", translateEnglishToMorseCode);
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  " ": " ",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "0": "-----"
+};
 
-  const translateMorseToEnglish = () => {
-    const morseInputValue = MorseInput().value;
-    
-    const englishOutput = document.getElementById("enOutput");
-    
-    const morseCharacters = morseInputValue.split(" ");
-    
-    const englishCharacters = morseCharacters.map(char => morseAlphabet[char]);
-    
-    const englishString = englishCharacters.join("");
-    
-    englishOutput.innerHTML = englishString;
-  };
+const morseAlphabet = {
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  " ": " ",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0"
+};
 
-  const MorseInput = () => document.getElementById("morseInput");
-  
-  document.getElementById("translateMorseCodeToEnglish").addEventListener("click", translateMorseToEnglish);
+const translateEnglishToMorseCode = () => {
+  const inputValue = input().value;
 
-// const compareTextToArray = (morseCode, displayText) => {
+  const output = document.getElementById("morseOutput");
 
-//     const displayTextLoop = displayText.map();
-//     document.getElementById("p").innerHTML(morseLoop);
-    
+  const characters = inputValue.toLowerCase().split("");
 
-// }
+  const morseCharacters = characters.map(char => alphabet[char]);
 
-// const morseLoop = (result) => { 
-//     return morseCode.char.includes(result);
-// }
+  const morseString = morseCharacters.join(" ");
 
+  output.innerHTML = morseString;
+};
 
-// compareTextToArray(morseCode, displayText);
+const input = () => document.getElementById("englishInput");
 
+document
+  .getElementById("translateEnglishToMorse")
+  .addEventListener("click", translateEnglishToMorseCode);
 
+const translateMorseToEnglish = () => {
+  const morseInputValue = MorseInput().value;
 
-// const getUserEmail = (people) => people.map(userEmail);
+  const englishOutput = document.getElementById("enOutput");
 
-// const userEmail = (person) =>  person.email;
+  const morseCharacters = morseInputValue.split(" ");
 
-// console.log(getUserEmail(people));
+  const englishCharacters = morseCharacters.map(char => morseAlphabet[char]);
 
+  const englishString = englishCharacters.join("");
 
+  englishOutput.innerHTML = englishString;
+};
 
-    
+const MorseInput = () => document.getElementById("morseInput");
+
+document
+  .getElementById("translateMorseCodeToEnglish")
+  .addEventListener("click", translateMorseToEnglish);
